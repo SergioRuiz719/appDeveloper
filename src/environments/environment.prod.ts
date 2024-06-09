0a9d65fd12e6ef638192beedd81170d4d8e97e10
SergioRuiz719 <sergio.ruiz@jpyrsa.mx>
2024-06-08 18:09:19 -0600

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
+// export const environment = {
+//   production: true
+// };
+
 export const environment = {
-  production: true
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDKpVsJPoh4Z68XPS7-flMZKINsyTx6SmM",
+    authDomain: "appdeveloper-5f55c.firebaseapp.com",
+    projectId: "appdeveloper-5f55c",
+    storageBucket: "appdeveloper-5f55c.appspot.com",
+    messagingSenderId: "987295637281",
+    appId: "1:987295637281:web:8d4ca3e006990801026860"
+  }
 };
